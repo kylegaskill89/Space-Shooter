7253879fa0a7b8815b8e39fe148f93b759f57973
Kyle Gaskill <kylegaskill64@yahoo.com>
2015-05-13 20:32:40 -0400

    Wrote Movement and Boundary Scripts
    Player Sprite is implemented as well as the ability to move left and
    right. This works with a controller as well. The player is bound inside
    of the view.

---
+++ Space Shooter/Assets/Scripts/ScreenBoundaries.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenBoundaries : MonoBehaviour {
+
+	public float shipBoundary = .3f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 pos = transform.position;
+
+
+		float screenRatio = (float)Screen.width / (float)Screen.height;
+		float orthoSize = Camera.main.orthographicSize * screenRatio;
+
+		if (pos.x + shipBoundary > orthoSize) {
+			pos.x = orthoSize - shipBoundary;
+		}
+		if (pos.x - shipBoundary < -orthoSize) {
+			pos.x = -orthoSize + shipBoundary;
+		}
+		transform.position = pos;
+	}
+}
