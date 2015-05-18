1b569a14e003b5ecf9dae710b75ef6221a74dec9
AnyWays655 <ErickVorpahl@gmail.com>
2015-05-17 22:53:47 -0400

    v0.32 - Electric Accordion
    Enemy Movement Error TEST DNUIFR

---
+++ Space Shooter/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		EnemyStats stats = GetComponent<EnemyStats>();
+
+		transform.Translate( new Vector3( 0, (stats.maxSpeed * -1) * Time.deltaTime, 0));
+
+	}
+}
